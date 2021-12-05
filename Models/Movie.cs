6f55d392899c13f284b546075a971133cb37dbab
Yosha Kunnummal <yoshakunnummal@rocketmortgage.com>
2021-12-04 22:23:04 -0500

    First commit

---
+++ Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieMVC.Models
+{
+    public enum Genre
+    {
+        Action,
+        Comedy,
+        Drama,
+        Horror,
+        Romance
+    }
+    public class Movie
+    {
+        public string ID { get; set; }
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+        public int Year { get; set; }
+        public string Actors { get; set; }
+        public string Directors { get; set; }
+    }
+}
